aa3347b038335ff09b8f8bc932c68b056a360f82
weinbergerlab <dweinber@gmail.com>
2019-05-05 21:27:05 -0400

    fix n times

--- functions.R
+++ functions.R
@@ -394,7 +394,7 @@ prev.lag.func<-function(ds){
     
     
   jags.func<-function(ds){
-      n.times<-nrow(ds[test1$id==1,])
+      n.times<-nrow(ds[ds$id==1,])
       ds$hh.id<-rep(1:(n.people/2), each=2*n.times)
       ds.split<-split(ds, ds$id)
       ds2<-lapply(ds.split, day.index.func )
@@ -428,14 +428,14 @@ prev.lag.func<-function(ds){
       } 
       
       for(k in 2:6){
-      prob_2_1[k]<- exp(beta_2_1*time.increment[k]/14) #give probaility for 14 days
-      prob_2_3[k]<- exp(beta_2_3*time.increment[k]/14)
-      prob_1_2[k]<- exp(beta_1_2*time.increment[k]/14)
+      prob_2_1[k]<- ilogit(beta_2_1)*time.increment[k]/14 #give probaility for 14 days
+      prob_2_3[k]<- prob_1_2[k] + ilogit(beta_within_hh)*time.increment[k]/14
+      prob_1_2[k]<- ilogit(beta_1_2)*time.increment[k]/14
       }
       #If truncate these priors at 0, it ensures that exp(beta_i_j) falls within the range [0,1]
       beta_1_2~dnorm(0,1e-2)T(,0) #log-acq rate
       beta_2_1~dnorm(0,1e-2)T(,0)
-      beta_2_3<- beta_1_2+ exp(beta_within_hh) #log_prob for 2_3 equal prob for 1_2 plus within HH effect
+      #beta_2_3<- beta_1_2+ exp(beta_within_hh) #log_prob for 2_3 equal prob for 1_2 plus within HH effect
       beta_within_hh ~ dnorm(0 ,1e-2)T(,0)
       switch<-c(2,1)
       #prob_within_hh_day<-exp(beta_2_3-beta_1_2 )
@@ -443,8 +443,8 @@ prev.lag.func<-function(ds){
     #exp(beta) gives prob change per 2 weeks
       prob_2_1_day<-exp(beta_2_1)/14
       prob_1_2_day<-exp(beta_1_2)/14
-      prob_2_3_day<-exp(beta_2_3)/14
-      logit_prob_within_hh_day<- logit(prob_2_3_day - prob_1_2_day)
+      #prob_2_3_day<-exp(beta_2_3)/14
+      #logit_prob_within_hh_day<- logit(prob_2_3_day - prob_1_2_day)
 
       }
       "
@@ -458,7 +458,7 @@ prev.lag.func<-function(ds){
              n.iter=5000) 
       
       posterior_samples<-coda.samples(model_jags, 
-                                      variable.names=c('prob_2_1_day', 'prob_2_3_day','prob_1_2_day','beta_2_1','beta_2_3','beta_1_2'),
+                                      variable.names=c('prob_2_1_day','prob_1_2_day','beta_2_1','beta_1_2','beta_within_hh'),
                                       
                                       thin=1,
                                       n.iter=5000)
